f8904f578de0691fe82d110a1076e65110304518
Najwarci <rachdi.najwa91@gmail.com>
2025-09-19 13:43:21 +0200

    task

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,9 @@
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+    // rien à faire ici
+    }
+    return [task, task2];
+  }
