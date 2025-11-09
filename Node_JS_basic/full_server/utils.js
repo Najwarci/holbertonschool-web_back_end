04b17c558e830f162e3f5dfa9f91ce8700c90715
Najwarci <rachdi.najwa91@gmail.com>
2025-11-09 15:12:54 +0100

    test

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+import fs from 'fs';
 
-function countStudents(path) {
+export default function readDatabase(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
+
       const lines = data.split('\n').filter((line) => line.trim() !== '');
       const students = lines.slice(1);
 
-      console.log(`Number of students: ${students.length}`);
-
       const fields = {};
+
       students.forEach((line) => {
         const parts = line.split(',');
         const firstname = parts[0].trim();
@@ -23,14 +23,8 @@ function countStudents(path) {
         }
         fields[field].push(firstname);
       });
-      const fieldOrder = Object.keys(fields);
-      fieldOrder.forEach((field) => {
-        console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
-        );
-      });
-      resolve();
+
+      resolve(fields);
     });
   });
 }
-module.exports = countStudents;
